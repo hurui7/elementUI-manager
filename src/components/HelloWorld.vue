cf2f38b0a4e4fee0b89331f6cc2d8cb8a3f5ec35
hurui7 <784814063@qq.com>
2020-01-17 00:00:15 +0800

    [fixed] 提交新项目，仅仅是搭了框架

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <el-col :span="4">2</el-col>
+    <el-col :span="20">3</el-col>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
